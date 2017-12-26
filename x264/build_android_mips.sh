4b7663f30fc21bf7e6e8c285a56ea8544c295a6a
Melonpi <melonpi1943@gmail.com>
2017-12-26 23:41:25 +0800

    Update build_android_mips.sh

--- x264/build_android_mips.sh
+++ x264/build_android_mips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-NDK=/Users/yesimroy/Library/Android/sdk/ndk-bundle
-PLATFORM=$NDK/platforms/android-18/arch-mips/
-TOOLCHAIN=$NDK/toolchains/mipsel-linux-android-4.9/prebuilt/darwin-x86_64
+NDK=E:/Android/sdk/ndk-bundle
+PLATFORM=$NDK/platforms/android-18/arch-arm/
+PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64
 PREFIX=./android/mips
 
 function build_one
