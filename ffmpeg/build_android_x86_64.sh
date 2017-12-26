134be2366547f407709d5f3828f585d39c58ab90
Melonpi <melonpi1943@gmail.com>
2017-12-26 23:19:53 +0800

    Update build_android_x86_64.sh

--- ffmpeg/build_android_x86_64.sh
+++ ffmpeg/build_android_x86_64.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Change NDK to your Android NDK location
-NDK=/Users/yesimroy/Library/Android/sdk/ndk-bundle
-PLATFORM=$NDK/platforms/android-21/arch-x86_64/
-PREBUILT=$NDK/toolchains/x86_64-4.9/prebuilt/darwin-x86_64
+NDK=E:/Android/sdk/ndk-bundle
+PLATFORM=$NDK/platforms/android-18/arch-arm/
+PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64
 
 GENERAL="\
 --enable-small \
